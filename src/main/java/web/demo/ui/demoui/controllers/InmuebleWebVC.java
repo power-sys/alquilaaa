38c1b7e413c484f3f07407c839525d1351528834
Nicolas Villalba <nerynicolas@gmail.com>
2018-10-30 09:29:19 -0300

    endpoint removed

--- src/main/java/web/demo/ui/demoui/controllers/InmuebleWebVC.java
+++ src/main/java/web/demo/ui/demoui/controllers/InmuebleWebVC.java
@@ -1,6 +1,5 @@
 package web.demo.ui.demoui.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import web.demo.ui.demoui.dtos.InmuebleDTO;
-//import com.powersys.Alquila.models.Inmueble;
+
 import web.demo.ui.demoui.services.implementations.InmuebleService;
 
 @Controller
@@ -24,12 +23,6 @@ public class InmuebleWebVC {
 	public String cargarInmuebleForm(Model model) {
 		model.addAttribute("inmueble", new InmuebleDTO());
 		return "cargarInmueble";
-	}
-
-	@GetMapping(value = "/listar/inmuebles")
-	public String listarInmueble(Model model) {
-		model.addAttribute("inmueble", this.inmuebleService.findAll());
-		return "listarInmuebles";
 	}
 
 	@GetMapping(value = "/")
