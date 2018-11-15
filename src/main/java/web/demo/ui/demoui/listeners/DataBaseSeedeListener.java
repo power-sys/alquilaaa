424807bbd7ee98af52bab0c86a8ffafdaad3c5bd
Nicolas Villalba <nerynicolas@gmail.com>
2018-11-15 08:16:58 -0300

    different dates to seed entities were added

--- src/main/java/web/demo/ui/demoui/listeners/DataBaseSeedeListener.java
+++ src/main/java/web/demo/ui/demoui/listeners/DataBaseSeedeListener.java
@@ -1,6 +1,9 @@
 package web.demo.ui.demoui.listeners;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,6 +34,28 @@ public class DataBaseSeedeListener {
 	@EventListener
 	public void seeder(ContextRefreshedEvent event) {
 		System.out.println("::::::::::::::::::::::::DB SEEDING:::::::::::::::::::::::::");
+		
+		List<LocalDate> dates = new ArrayList<>();
+		dates.add(LocalDate.of(2018, Month.NOVEMBER, 3));
+		dates.add(LocalDate.of(2018, Month.NOVEMBER, 7));
+		dates.add(LocalDate.of(2018, Month.NOVEMBER, 8));
+		dates.add(LocalDate.of(2018, Month.NOVEMBER, 12));
+		dates.add(LocalDate.of(2018, Month.NOVEMBER, 2));
+		dates.add(LocalDate.of(2018, Month.NOVEMBER, 5));
+		dates.add(LocalDate.of(2018, Month.OCTOBER, 30));
+		dates.add(LocalDate.of(2018, Month.OCTOBER, 21));
+		dates.add(LocalDate.of(2018, Month.OCTOBER, 22));
+		dates.add(LocalDate.of(2018, Month.OCTOBER, 2));
+		dates.add(LocalDate.of(2018, Month.SEPTEMBER, 3));
+		dates.add(LocalDate.of(2018, Month.SEPTEMBER, 1));
+		dates.add(LocalDate.of(2018, Month.SEPTEMBER, 10));
+		dates.add(LocalDate.of(2018, Month.SEPTEMBER, 13));
+		dates.add(LocalDate.of(2018, Month.AUGUST, 29));
+		dates.add(LocalDate.of(2018, Month.AUGUST, 16));
+		dates.add(LocalDate.of(2018, Month.AUGUST, 8));
+		dates.add(LocalDate.of(2018, Month.AUGUST, 3));
+		dates.add(LocalDate.of(2018, Month.AUGUST, 19));
+		dates.add(LocalDate.of(2018, Month.JUNE, 26));
 		
 		for (int i = 0; i < 20; i++) {
 			BaseProducer fake = fakingService.getFaker().baseProducer();
@@ -82,6 +107,7 @@ public class DataBaseSeedeListener {
 			entity.setPrice(amount);
 			entity.setType((i % 2 == 0)? PlaceType.DEPARMENT : PlaceType.HOUSE);
 			entity.addImage(fakeText.latinSentence(10));
+			entity.setPublished(dates.get(i));
 			placeRepo.save(entity);
 		}
 		
