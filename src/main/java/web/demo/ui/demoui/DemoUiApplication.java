a0cf8b559470ba2d56fee843c38cc73688212b56
Nicolas Villalba <nerynicolas@gmail.com>
2018-11-19 06:21:08 -0300

    thymeleaf configuration was added

--- src/main/java/web/demo/ui/demoui/DemoUiApplication.java
+++ src/main/java/web/demo/ui/demoui/DemoUiApplication.java
@@ -2,10 +2,17 @@ package web.demo.ui.demoui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
 
 
 @SpringBootApplication(scanBasePackages = {"web.demo.ui.demoui"})
 public class DemoUiApplication {
+	
+	@Bean
+    public Java8TimeDialect java8TimeDialect() {
+        return new Java8TimeDialect();
+    }
 	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoUiApplication.class, args);
