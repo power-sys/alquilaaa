393422362b3ba4289ca1069d35bbd48e49c16462
Nicolas Villalba <nerynicolas@gmail.com>
2018-11-20 11:39:28 -0300

    InmuebleDTO was replaced for ImageDTO that extends it and add a field
    for managing multipart files, also StorageService was added to return
    
    a reference for the image stored, to been assigned again to ImageDTO

--- src/main/java/web/demo/ui/demoui/controllers/InmuebleWebVC.java
+++ src/main/java/web/demo/ui/demoui/controllers/InmuebleWebVC.java
@@ -1,5 +1,8 @@
 package web.demo.ui.demoui.controllers;
 
+import java.util.List;
+
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,17 +11,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import web.demo.ui.demoui.domain.Property;
+import web.demo.ui.demoui.dtos.ImageDTO;
 import web.demo.ui.demoui.dtos.InmuebleDTO;
-
-import web.demo.ui.demoui.services.implementations.InmuebleService;
+import web.demo.ui.demoui.services.StorageService;
+import web.demo.ui.demoui.services.implementations.InmuebleWhitImageService;
 
 @Controller
 public class InmuebleWebVC {
 
-	private InmuebleService inmuebleService;
+	private InmuebleWhitImageService inmuebleService;
+	
+	private StorageService image;
 
-	public InmuebleWebVC(InmuebleService service) {
+	public InmuebleWebVC(InmuebleWhitImageService service, StorageService image) {
 		this.inmuebleService = service;
+		this.image = image;
 	}
 
 	@RequestMapping(value = "/cargarInmueble", method = RequestMethod.GET)
@@ -28,14 +36,18 @@ public class InmuebleWebVC {
 	}
 	
 	@PostMapping("/cargarInmueble")
-	public String altaInmueble(@ModelAttribute InmuebleDTO inmuebleDTO, Model model) {
-		this.inmuebleService.insertInmueble(inmuebleDTO);
+	public String altaInmueble(@ModelAttribute ImageDTO imageDTO) {
+		imageDTO.setPath(this.image.store(imageDTO.getImage()));
+		this.inmuebleService.insertInmueble(imageDTO);
 		return "redirect:/";
 	}
 
 	@GetMapping(value = "/")
 	public String home(Model model) {
-		model.addAttribute("inmueble", this.inmuebleService.findAll());
+		List<Property> ps = this.inmuebleService.findAll();
+		model.addAttribute("inmueble", ps);
 		return "home1";
 	}
+	
+	
 }
